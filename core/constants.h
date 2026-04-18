fe0a81fafaa79b2ebf1dc7ea017c8e79c57feaac
Jony7648 <sonicracer2006@gmail.com>
2026-04-18 10:14:43 -0400

    working on adding the icon to windows

--- core/constants.h
+++ core/constants.h
@@ -1,5 +1,5 @@
 #pragma once
 #include <iostream>
 
-const std::string CSS_STYLES_DIR = "gtk_componets/css_styles/";
-const unsigned short MAX_RECURSIONS = 100;
+#define CSS_STYLES_DIR "css_styles/"
+#define MAX_RECURSIONS 100
